1c0a5a3c4b1747f7b0a83a0a9db80683978af4eb
Pulkituix <pulkit@uixlabs.in>
2025-05-27 12:46:50 +0000

    objects and format of API

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,27 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor
+// const {courseInstructor} = course;
+// console.log(courseInstructor);
+// const {courseInstructor: ci} = course;
+// console.log(ci);
+
+// ******API's******//
+
+// {
+//     "":"",
+//     "":""
+// } // JSON format
+
+// [
+//     {
+
+//     },
+//     {
+
+//     }
+// ] // array of objects
