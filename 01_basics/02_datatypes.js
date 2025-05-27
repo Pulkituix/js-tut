bbdafad6aced1c7043d6dc8ad7325d7cb9b29700
Pulkituix <pulkit@uixlabs.in>
2025-05-27 04:28:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using node.js, not browser
+
+//alert("hello"); // we are using node.js, not browser
+
+let name = "Pulkit"; // string
+
+let age = 18; // Number // bigint
+
+let isLoggedIn = true; // bool
+
+let state = null // null => standalone value
+
+let und; // undefined
+
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Pulkit"); // string
+
+console.log(typeof name); // string
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
