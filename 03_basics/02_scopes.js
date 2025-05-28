fe0ad280d6cdda0807ef243c49afd0b4e8f6d91a
Pulkituix <pulkit@uixlabs.in>
2025-05-28 06:17:01 +0000

    scopes and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,47 @@ if(true){
     let a = 10;
     const b = 20;
     var c = 30;
+    // console.log("Inner", a);
 }
 
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Pulkit";
+
+    function two(){
+        const website = "Youtube";
+        // console.log(username);
+    }
+    // console.log(website); // error
+
+    two();
+}
+// one();
+
+if(true){
+    const username = "Pulkit";
+    if(username == "Pulkit"){
+        const website = "Youtube";
+        // console.log(username + website);
+    }
+    // console.log(website); // error
+}
+// console.log(username); // error
+
+
+// *********************** //
+
+// addone(5); // correct
+function addone(num){
+    return num+1;
+}
+addone(5);
+
+// addTwo(5); // error
+const addTwo = function(num){
+    return num+2;
+}
+addTwo(5);
